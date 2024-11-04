0b83556d8b1cfb111868d4d07ff1a8891bca6d2d
etleyden <etleyden@gmail.com>
2024-11-04 15:41:43 -0500

    set up react

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: './',
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+};
+
