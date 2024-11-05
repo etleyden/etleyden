22ae68a07fc9d98902dff15f8b3102f14a37ed13
etleyden <etleyden@gmail.com>
2024-11-05 08:40:13 -0500

    active navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router,  Link } from 'react-router-dom';
+import { BrowserRouter as Router,  NavLink} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/navbar.css';
+
+
 
 const Navbar: React.FC = () => (
     <header className='row sticky-top' id='navbar'>
         <div id="navbar_div" className="container d-flex flex-column py-4">
           <nav className="nav nav-masthead justify-content-evenly float-md-end">
-            <Link className="nav-link fw-bold py-1 px-0" aria-current="page" to="/">Home</Link>
-            <Link className="nav-link fw-bold py-1 px-0" to="/resume">Resume</Link>
-            <Link className="nav-link fw-bold py-1 px-0" to="/contact">Contact</Link>
+            <NavLink id="homelink" className="nav-link fw-bold py-1 px-0" aria-current="page" to="/">Home</NavLink>
+            <NavLink id="resumelink" className="nav-link fw-bold py-1 px-0" to="/resume">Resume</NavLink>
+            <NavLink id="contactlink" className="nav-link fw-bold py-1 px-0" to="/contact">Contact</NavLink>
           </nav>
         </div>
       </header>
